8ea57ea7124327d837fffbaf23cf68ac4f5eaee3
Daniil Yuzhanin <Daniil_Iuzhanin@epam.com>
2022-04-05 15:56:15 +0300

    feat: added router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       Ola!
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <CssBaseline />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
