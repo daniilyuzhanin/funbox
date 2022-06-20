21c6357810619f27bae2d88ebd94e46919923668
Daniil Yuzhanin <Daniil_Iuzhanin@epam.com>
2022-06-20 15:15:29 +0300

    feat: created layout, added variables to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { createTheme, ThemeProvider, Theme } from '@mui/material/styles';
 import { Layout } from 'common/components/Layout';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+declare module '@mui/styles/defaultTheme' {
+  interface DefaultTheme extends Theme {}
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#fff',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    // secondary: {
+    //   main: '#11cb5f',
+    // },
+  },
+  spacing: 2,
+});
+
 const App = () => (
   <BrowserRouter>
-    <Layout />
+    <ThemeProvider theme={theme}>
+      <Layout />
+    </ThemeProvider>
   </BrowserRouter>
 );
 
--- src/common/components/Layout/Layout.tsx
+++ src/common/components/Layout/Layout.tsx
@@ -1,17 +1,56 @@
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import React, { PropsWithChildren } from 'react';
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    minHeight: '100vh',
+    background: `url(${'/pattern.jpeg'}) center center fixed`,
+    padding: theme.spacing(18, 0, 33.5),
+    position: 'relative',
+    zIndex: 1,
+    color: theme.palette.primary.light,
+
+    '&:before': {
+      content: '""',
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: '100%',
+      height: '100%',
+      background: 'linear-gradient(to bottom,rgba(0,0,0,.5)0%,rgba(0,0,0,0)50%,rgba(0,0,0,.5)100%)',
+      zIndex: 2,
+    },
+  },
+  layout: {
+    position: 'relative',
+    zIndex: 3,
+  },
+  layoutTitleText: {
+    fontFamily: '"Exo 2", sans-serif',
+    fontSize: theme.spacing(18),
+    fontWeight: 100,
+    textShadow: '0px 1px 1px #000000',
+    paddingBottom: theme.spacing(12),
+  },
+}));
 
 export const Layout = ({ children }: PropsWithChildren<{}>) => {
   const classes = useStyles();
+  const {
+    wrapper,
+    layout,
+    layoutTitleText,
+  } = classes;
 
   return (
-    <Grid container>
-      <Grid item>Ты сегодня покормил кота?</Grid>
-      <Grid container>
-        Card container!
+    <Grid container className={wrapper}>
+      <Grid container direction="column" className={layout}>
+        <Grid item textAlign="center" className={layoutTitleText}>Ты сегодня покормил кота?</Grid>
+        <Grid container justifyContent="center">
+          <Grid item>Card container!</Grid>
+          <Box style={{ textShadow: '0px 1px 1px #000000' }}>Ты сегодня покормил кота?</Box>
+        </Grid>
       </Grid>
     </Grid>
   );
